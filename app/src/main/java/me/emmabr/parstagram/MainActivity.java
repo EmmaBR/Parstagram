50479b700378d00712a4e493a329a361fea98328
Emma Rivera <erive011@ucr.edu>
2018-07-07 16:27:54 -0700

    First Commit

---
+++ app/src/main/java/me/emmabr/parstagram/MainActivity.java
@@ -0,0 +1,20 @@
+package me.emmabr.parstagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
